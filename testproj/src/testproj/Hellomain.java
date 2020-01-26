ad3c140ae063717004bd04947d8eb76b74ae59f4
riensolutions <riensolutions@mymacbookpro>
2020-01-26 14:34:39 +0530

    Newtestproj

---
+++ testproj/src/testproj/Hellomain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package testproj;
+
+/**
+ * @author riensolutions
+ *
+ */
+public class Hellomain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
